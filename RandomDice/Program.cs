832174f518e1c883fac4b27ea5d09efca722c1bb
HenriqueMonteiro <Henriquepupet@gmail.com>
2023-03-31 14:38:47 +0100

    Finished the exercise

---
+++ RandomDice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RandomDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please write the number of dices: ");
+
+            int numdice = Convert.ToInt32(Console.ReadLine());
+            int sumdice = 0;
+            Random rnd = new Random();
+
+            for(int i=0; i < numdice; i++)
+            {                
+                sumdice += rnd.Next(1,6);
+            }
+
+            Console.WriteLine("The sum number of the dices is: " + sumdice);
+            
+ 
+            
+        }
+    }
+}
